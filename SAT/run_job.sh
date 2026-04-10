d15860fc9aba2d26921f1db04c09a0232402b3f6
Aditya Jain <adityaj8@vision-h01.cs.illinois.edu>
2026-04-09 23:03:24 -0500

    Results for N=28

--- SAT/run_job.sh
+++ SAT/run_job.sh
@@ -12,4 +12,4 @@ cd /home/adityaj8/k4free/SAT
 mkdir -p logs
 
 # Run the production sweep
-python -m k4free_ilp.run_production --workers 24 25 26 27 28 29 30 31 32 33 34 35 "$@"
+python -m k4free_ilp.run_production --workers 16 24 "$@"
